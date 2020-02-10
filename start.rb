19e730e6719ee60772c957cfbb5907b60310ebd0
Alex Robinson <alexandra.scott335@gmail.com>
2020-02-10 10:32:34 -0700

    add more complexity to start.rb

--- start.rb
+++ start.rb
@@ -7,12 +7,15 @@ print ">>"
 
 date = gets.chomp
 astroid_details = NearEarthObjects.find_neos_by_date(date)
+astroid_list = astroid_details[:astroid_list]
+total_number_of_astroids = astroid_details[:total_number_of_astroids]
+largest_astroid = astroid_details[:biggest_astroid]
 
 column_labels = { name: "Name", diameter: "Diameter", miss_distance: "Missed The Earth By:" }
 column_data = column_labels.each_with_object({}) do |(col, label), hash|
   hash[col] = {
     label: label,
-    width: [astroid_details.map { |astroid| astroid[col].size }.max, label.size].max}
+    width: [astroid_list.map { |astroid| astroid[col].size }.max, label.size].max}
 end
 
 header = "| #{ column_data.map { |_,col| col[:label].ljust(col[:width]) }.join(' | ') } |"
@@ -29,8 +32,10 @@ end
 
 formated_date = DateTime.parse(date).strftime("%A %b %d, %Y")
 puts "______________________________________________________________________________"
-puts "\nHere is a list of astroids that almost hit the earth on #{formated_date}."
+puts "On #{formated_date}, there were #{total_number_of_astroids} objects that almost collided with the earth."
+puts "The largest of these was #{largest_astroid} ft. in diameter."
+puts "\nHere is a list of objects with details:"
 puts divider
 puts header
-create_rows(astroid_details, column_data)
+create_rows(astroid_list, column_data)
 puts divider
