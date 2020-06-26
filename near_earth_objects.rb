b36c7acf5947ea93b077a26565f9db096d0b240f
Chase.Gaddis <cgaddis36@gmail.com>
2020-06-26 13:44:32 -0600

    fix asteroid spelling mistake

--- near_earth_objects.rb
+++ near_earth_objects.rb
@@ -11,27 +11,28 @@ class NearEarthObjects
       url: 'https://api.nasa.gov',
       params: { start_date: date, api_key: ENV['nasa_api_key']}
     )
+
     asteroids_list_data = conn.get('/neo/rest/v1/feed')
 
     parsed_asteroids_data = JSON.parse(asteroids_list_data.body, symbolize_names: true)[:near_earth_objects][:"#{date}"]
 
-    largest_astroid_diameter = parsed_asteroids_data.map do |astroid|
-      astroid[:estimated_diameter][:feet][:estimated_diameter_max].to_i
+    largest_asteroid_diameter = parsed_asteroids_data.map do |asteroid|
+      asteroid[:estimated_diameter][:feet][:estimated_diameter_max].to_i
     end.max { |a,b| a<=> b}
 
-    total_number_of_astroids = parsed_asteroids_data.count
-    formatted_asteroid_data = parsed_asteroids_data.map do |astroid|
+    total_number_of_asteroids = parsed_asteroids_data.count
+    formatted_asteroid_data = parsed_asteroids_data.map do |asteroid|
       {
-        name: astroid[:name],
-        diameter: "#{astroid[:estimated_diameter][:feet][:estimated_diameter_max].to_i} ft",
-        miss_distance: "#{astroid[:close_approach_data][0][:miss_distance][:miles].to_i} miles"
+        name: asteroid[:name],
+        diameter: "#{asteroid[:estimated_diameter][:feet][:estimated_diameter_max].to_i} ft",
+        miss_distance: "#{asteroid[:close_approach_data][0][:miss_distance][:miles].to_i} miles"
       }
     end
 
     {
-      astroid_list: formatted_asteroid_data,
-      biggest_astroid: largest_astroid_diameter,
-      total_number_of_astroids: total_number_of_astroids
+      asteroid_list: formatted_asteroid_data,
+      biggest_asteroid: largest_asteroid_diameter,
+      total_number_of_asteroids: total_number_of_asteroids
     }
   end
 end
--- start.rb
+++ start.rb
@@ -1,21 +1,21 @@
 require_relative 'near_earth_objects'
 
 puts "________________________________________________________________________________________________________________________________"
-puts "Welcome to NEO. Here you will find information about how many meteors, astroids, comets pass by the earth every day. \nEnter a date below to get a list of the objects that have passed by the earth on that day."
+puts "Welcome to NEO. Here you will find information about how many meteors, asteroids, comets pass by the earth every day. \nEnter a date below to get a list of the objects that have passed by the earth on that day."
 puts "Please enter a date in the following format YYYY-MM-DD."
 print ">>"
 
 date = gets.chomp
-astroid_details = NearEarthObjects.find_neos_by_date(date)
-astroid_list = astroid_details[:astroid_list]
-total_number_of_astroids = astroid_details[:total_number_of_astroids]
-largest_astroid = astroid_details[:biggest_astroid]
+asteroid_details = NearEarthObjects.find_neos_by_date(date)
+asteroid_list = asteroid_details[:asteroid_list]
+total_number_of_asteroids = asteroid_details[:total_number_of_asteroids]
+largest_asteroid = asteroid_details[:biggest_asteroid]
 
 column_labels = { name: "Name", diameter: "Diameter", miss_distance: "Missed The Earth By:" }
 column_data = column_labels.each_with_object({}) do |(col, label), hash|
   hash[col] = {
     label: label,
-    width: [astroid_list.map { |astroid| astroid[col].size }.max, label.size].max}
+    width: [asteroid_list.map { |asteroid| asteroid[col].size }.max, label.size].max}
 end
 
 header = "| #{ column_data.map { |_,col| col[:label].ljust(col[:width]) }.join(' | ') } |"
@@ -26,16 +26,16 @@ def format_row_data(row_data, column_info)
   puts "| #{row} |"
 end
 
-def create_rows(astroid_data, column_info)
-  rows = astroid_data.each { |astroid| format_row_data(astroid, column_info) }
+def create_rows(asteroid_data, column_info)
+  rows = asteroid_data.each { |asteroid| format_row_data(asteroid, column_info) }
 end
 
 formated_date = DateTime.parse(date).strftime("%A %b %d, %Y")
 puts "______________________________________________________________________________"
-puts "On #{formated_date}, there were #{total_number_of_astroids} objects that almost collided with the earth."
-puts "The largest of these was #{largest_astroid} ft. in diameter."
+puts "On #{formated_date}, there were #{total_number_of_asteroids} objects that almost collided with the earth."
+puts "The largest of these was #{largest_asteroid} ft. in diameter."
 puts "\nHere is a list of objects with details:"
 puts divider
 puts header
-create_rows(astroid_list, column_data)
+create_rows(asteroid_list, column_data)
 puts divider
