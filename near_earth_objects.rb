8b5c8c0b749c3c8b0d87aaac9d188808e095abfa
Eduardo Parra <helloeduardo@icloud.com>
2020-11-16 10:54:38 -0700

    Separate out concerns into new methods

--- near_earth_objects.rb
+++ near_earth_objects.rb
@@ -2,6 +2,7 @@ require 'faraday'
 require 'figaro'
 require 'pry'
 require_relative 'nasa_service'
+require_relative 'asteroid'
 
 # Load ENV vars via Figaro
 Figaro.application = Figaro::Application.new(environment: 'production', path: File.expand_path('../config/application.yml', __FILE__))
@@ -9,29 +10,10 @@ Figaro.load
 
 class NearEarthObjects
   def self.find_neos_by_date(date)
-    # conn = Faraday.new(
-    #   url: 'https://api.nasa.gov',
-    #   params: { start_date: date, api_key: ENV['nasa_api_key']}
-    # )
-    # asteroids_list_data = conn.get('/neo/rest/v1/feed')
-    asteroids_list_data = NASAService.asteroids(date)
-    # require "pry"; binding.pry
-    # Hash of dates in the week following the date with asteroids data in array
+    asteroids_data = NASAService.asteroids(date)
 
-    # parsed_asteroids_data = JSON.parse(asteroids_list_data.body, symbolize_names: true)[:near_earth_objects][:"#{date}"]
-    parsed_asteroids_data = asteroids_list_data[:"#{date}"]
-
-    largest_asteroid_diameter = parsed_asteroids_data.map do |asteroid|
-      asteroid[:estimated_diameter][:feet][:estimated_diameter_max].to_i
-    end.max { |a,b| a<=> b}
-
-    total_number_of_asteroids = parsed_asteroids_data.count
-    formatted_asteroid_data = parsed_asteroids_data.map do |asteroid|
-      {
-        name: asteroid[:name],
-        diameter: "#{asteroid[:estimated_diameter][:feet][:estimated_diameter_max].to_i} ft",
-        miss_distance: "#{asteroid[:close_approach_data][0][:miss_distance][:miles].to_i} miles"
-      }
+    @asteroids = asteroids_data.map do |asteroid_data|
+      Asteroid.new(asteroid_data)
     end
 
     {
@@ -39,5 +21,25 @@ class NearEarthObjects
       biggest_asteroid: largest_asteroid_diameter,
       total_number_of_asteroids: total_number_of_asteroids
     }
+  end
+
+  def self.formatted_asteroid_data
+    @asteroids.map do |asteroid|
+      {
+        name: asteroid.name,
+        diameter: "#{asteroid.diameter} ft",
+        miss_distance: "#{asteroid.miss_distance} miles"
+      }
+    end
+  end
+
+  def self.largest_asteroid_diameter
+    @asteroids.max_by do |asteroid|
+      asteroid.diameter
+    end.diameter
+  end
+
+  def self.total_number_of_asteroids
+    @asteroids.count
   end
 end
