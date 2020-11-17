c10fb0117632a162fb856400528944c9917cdec3
Eduardo Parra <helloeduardo@icloud.com>
2020-11-17 14:04:54 -0700

    Remove pry

--- near_earth_objects_test.rb
+++ near_earth_objects_test.rb
@@ -6,7 +6,6 @@ require_relative 'near_earth_objects'
 class NearEarthObjectsTest < Minitest::Test
   # def test_a_date_returns_a_list_of_neos
   #   results = NearEarthObjects.find_neos_by_date('2019-03-30')
-  #   require "pry"; binding.pry
   #   assert_equal '(2019 GD4)', results[:asteroid_list][0][:name]
   # end
 
