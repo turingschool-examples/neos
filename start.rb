a320dbf667011ea068915fff1c86a1d8bc5524e9
Carson Jardine <carsonjardine@gmail.com>
2020-11-27 15:57:12 -0700

    Initial changes and ideas

--- start.rb
+++ start.rb
@@ -1,11 +1,15 @@
 require_relative 'near_earth_objects'
 
+## PUT IN OWN VIEW CLASS?
 puts "________________________________________________________________________________________________________________________________"
 puts "Welcome to NEO. Here you will find information about how many meteors, astroids, comets pass by the earth every day. \nEnter a date below to get a list of the objects that have passed by the earth on that day."
 puts "Please enter a date in the following format YYYY-MM-DD."
 print ">>"
 
 date = gets.chomp
+
+#SPELL ASTEROID CORRECTLY
+#CREATE ASTEROID OBJECT?
 astroid_details = NearEarthObjects.find_neos_by_date(date)
 astroid_list = astroid_details[:astroid_list]
 total_number_of_astroids = astroid_details[:total_number_of_astroids]
